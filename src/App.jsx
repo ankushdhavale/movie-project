23de55a4ef39bc59fcaa5052cc33150e1abd3147
Ankush Kailas Dhavale <dhavaleankush7@gmail.com>
2024-03-13 00:27:35 +0530

    make search feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import { all } from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 
 function App() {
 
+  const [searchInput , setSearchInput] = useState();
+  const [allMovieList , setAllMovieList] = useState();
+  const [noMovieList , setNoMovieList] = useState(false);
+
+  const handelInput = (event) =>{
+    setSearchInput(event.target.value);
+  }
+
+  const fetchAllMovie = (searcht) => {
+    fetch(`https://www.omdbapi.com/?s=${searcht}&page=2&apikey=e98286fa`)
+    .then(res => res.json())
+    .then(result => setAllMovieList(result)) 
+  }
+
+  useEffect(()=>{
+    fetchAllMovie(searchInput);
+  },[]);
+   const {Title} = allMovieList.Search
+  console.log(Title);
   return (
     <>
-      hello
+    <div>
+      <input type="text" 
+      onChange={handelInput}/>
+      <div>
+        
+      </div>
+    </div>
     </>
   )
 }
